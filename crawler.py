dde390b061a756f8bba5bf6c57474970d063bdfa
Robson Gomes <fgomes.robson@gmail.com>
2015-05-19 01:26:49 -0300

    *

--- crawler.py
+++ crawler.py
@@ -1,8 +1,14 @@
 import requests as r
 import BeautifulSoup
+import os, csv
 
 r = r.get('http://ego.globo.com/famosos/tudo-sobre/abigail-breslin.html')
 soup = BeautifulSoup.BeautifulSoup(r.content)
 
 for a in soup.findAll('li', attrs={'class': 'aniversario'}):
     print(a.text)
+
+with  open("dados/links.csv") as links:
+    reader = csv.reader(links)
+    for row in reader:
+        print(row[0])
